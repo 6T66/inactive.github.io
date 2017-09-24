523b8c85cc207e3c208d67ed2aed87e37d57a6c2
6T66 <31939442+6T66@users.noreply.github.com>
2017-09-24 00:14:29 -0500

    Changed To Table
    Sorted into table, later will create rent list within list or sublist.

---
+++ ValueIncrementScript.js
@@ -0,0 +1,133 @@
+var mainValue = 2000;
+
+function mainValueReturnFunction() {
+	
+alert(mainValue);	
+	
+}
+
+function buttonFunction0() {
+	
+mainValue = mainValue + 200;
+	
+}
+function luxtaxFunction() {
+	
+mainValue = mainValue - 75;
+	
+}
+
+function inctaxFunction() {
+	
+mainValue = mainValue - 200;
+	
+}
+
+function medbuyFunction() {
+	
+mainValue = mainValue - 60;
+	
+}
+
+function smallblueFunction() {
+	
+mainValue = mainValue -100;
+	
+}
+
+function largeblueFunction() {
+	
+mainValue = mainValue - 120;
+	
+}
+
+function smapurFunction() {
+	
+mainValue = mainValue - 140;
+	
+}
+
+function larpurFunction() {
+	
+mainValue = mainValue - 160;
+	
+}
+
+function smaoraFunction() {
+	
+mainValue = mainValue - 180;
+	
+}
+
+function laroraFunction() {
+	
+mainValue = mainValue - 200;
+	
+}
+
+function smaredFunction() {
+	
+mainValue = mainValue - 220;
+	
+}
+
+function larredFunction() {
+	
+mainValue = mainValue - 240;
+	
+}
+
+function smayelFunction() {
+	
+mainValue = mainValue - 260;
+	
+}
+
+function laryelFunction() {
+	
+mainValue = mainValue - 280;
+	
+}
+
+function smagreFunction() {
+	
+mainValue = mainValue - 300;
+	
+}
+
+function largreFunction() {
+	
+mainValue = mainValue - 320;
+	
+}
+
+function parkFunction() {
+	
+mainValue = mainValue - 350;
+	
+}
+
+function boardFunction() {
+	
+mainValue = mainValue - 400;
+	
+}
+
+function railFunction() {
+
+mainValue = mainValue - 200;
+
+}
+
+function utilFunction() {
+	
+mainValue = mainValue - 150;
+	
+}
+
+function SDVFunction() {
+	
+var subtract = prompt("How much to be taken off?");
+mainValue = mainValue - subtract;
+
+}
